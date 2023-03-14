e26b8729ee282e814be6a77dee45a5c30703cc60
CristiSavin98 <81654056+CristiSavin98@users.noreply.github.com>
2023-03-14 15:19:22 +0000

    Fixes Ibft

--- Blockchain/src/main/java/group13/blockchain/consensus/IBFT.java
+++ Blockchain/src/main/java/group13/blockchain/consensus/IBFT.java
@@ -7,6 +7,8 @@ import group13.channel.bestEffortBroadcast.events.BEBSend;
 import group13.primitives.*;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -21,17 +23,21 @@ public class IBFT implements EventListener{
     private Lock lock = new ReentrantLock();
 
     private int prepared, commited;
-    private HashMap<String, Integer> prepares = new HashMap<>();
-    private HashMap<String, Integer> commits = new HashMap<>();
+    private HashMap<String, Set<Integer>> prepares = new HashMap<>();
+    private HashMap<String, Set<Integer>> commits = new HashMap<>();
     
     //Timer (eventually)
 
-    public IBFT(int id, int n, int f) {
+    private int leader;
+
+    public IBFT(int id, int n, int f, int leader, int port) {
         pId = id;
         nrProcesses = n;
         byzantineP = f;
         quorum = (nrProcesses + byzantineP)/2 + 1;
-        broadcast = new BEBroadcast(pId, new Address(5000 + pId));
+        this.leader = leader;
+        broadcast = new BEBroadcast(pId, new Address(port));
+        broadcast.subscribeDelivery(this);
     }
 
     private int leader(int instance, int round) {
@@ -46,10 +52,10 @@ public class IBFT implements EventListener{
         preparedRound = -1;
         preparedValue = null;
 
-        if ( leader(instance, round) == pId ) {
+        if ( leader == pId ) {
             //broadcast
             ///Message -> PRE_PREPARE, instance, round, input
-            // broadcast.update(new BEBSend("PRE_PREPARE\n" + instance + "\n" + round + "\n" + input));
+            broadcast.send(new BEBSend("PRE_PREPARE\n" + instance + "\n" + round + "\n" + input));
             //add signature ???
         }
 
@@ -71,10 +77,10 @@ public class IBFT implements EventListener{
             //verify signature
             //check round matches
             //here verify leader using params[2] or round??
-            if( msgType.equals("PRE_PREPARE") && leader(params[1], round) == src) {
+            if( msgType.equals("PRE_PREPARE") && leader == src && round == Integer.parseInt(params[2])) {
                 //what else to verify if valid besides signature??
                 prePrepare(params, src);
-            } else if (msgType.equals("PREPARE")) {
+            } else if (msgType.equals("PREPARE") && round == Integer.parseInt(params[2])) {
                 prepare(params, src);
             } else if (msgType.equals("COMMIT")) {
                 commit(params, src);
@@ -93,22 +99,25 @@ public class IBFT implements EventListener{
     private void prepare(String[] params, int src) {
 
         String key = params[1]+params[2]+params[3];
-
+        Set<Integer> setPrepares;
         if(! prepares.containsKey(key) ) {
-            prepares.put(key, 0);
+            setPrepares = new HashSet<Integer>();
+            prepares.put(key, setPrepares);
         }
         
-        int prepareCount = prepares.get(key);
+        setPrepares = prepares.get(key);
+        int prepareCount = setPrepares.size();
         
         if (prepareCount < quorum) {
 
-            prepareCount += 1;
-            prepares.put(key, prepareCount);
+            setPrepares.add(src);
+            prepareCount = setPrepares.size();
+            prepares.put(key, setPrepares);
             
 
             if( prepareCount >= quorum ) {
     
-                preparedRound = params[2];
+                preparedRound = Integer.parseInt(params[2]);
                 preparedValue = params[3];
                 broadcast.update(new BEBSend("COMMIT\n" + params[1] + "\n" + params[2] + "\n" + params[3]));
             }
@@ -117,21 +126,27 @@ public class IBFT implements EventListener{
 
     private void commit(String[] params, int src) {
 
+        Set<Integer> setCommits;
         String key = params[1]+params[2]+params[3];
 
         if(! commits.containsKey(key) ) {
-            commits.put(key, 0);
+            setCommits = new HashSet<Integer>();
+            commits.put(key, setCommits);
         }
-
-        int commitCount = commits.get(key);
+        setCommits = commits.get(key);
+        int commitCount = setCommits.size();
 
         if (commitCount < quorum) {
-            commitCount += 1;
-            commits.put(key, commitCount);
+            
+            setCommits.add(src);
+            commitCount = setCommits.size();
+            commits.put(key, setCommits);
     
             if( commitCount >= quorum) {
                 //timer stuff
-                // DECIDE( params[1], params[3], Qcommit?? );
+                System.out.println("DECIDE");
+                System.out.println("Value decided: " + params[3]);
+                // DECIDE( params[1], params[3], commits.get(key));
             }
         }
     }
